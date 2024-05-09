b1523990eb8765573b44d309c236157cda1b68f3
Zayyan <qureshizayyan49@gmail.com>
2024-05-09 06:45:54 +0530

    added some changes

--- script.js
+++ script.js
@@ -17,28 +17,25 @@ const ConvertToCelsius = (response) => {
     return Math.round(response - 273.15);
 }
 
-const DegreeSymbol = () => {
-    return `&deg;C`
-}
+const FillDataInCard = async (cityName) => {
 
-const FillDataInCard = (cityName) => {
-    data(cityName)
-        .then(response => response.json())
-        .then(response => {
-            degree.innerHTML = `${ConvertToCelsius(response.main.temp)}${DegreeSymbol()}`;
-            humidity.innerHTML = `Humidity: ${(response.main.humidity) / 100 * 100}%`;
-            feelslike.innerHTML = `Feels like: ${ConvertToCelsius(response.main.feels_like)}${DegreeSymbol()}`;
-            maxtemp.innerHTML = `Maximum Temperature: ${ConvertToCelsius(response.main.temp_max)}${DegreeSymbol()}`;
-            mintemp.innerHTML = `Minimum Temperature: ${ConvertToCelsius(response.main.temp_min)}${DegreeSymbol()}`;
-            windspeed.innerHTML = `Wind Speed: ${Math.round(response.wind.speed * 3.6)} km/h`;
-            console.log(response);
-        })
-        .catch(error => {
-            console.error(error);
-            document.querySelectorAll('.all').forEach((e) => e.innerHTML = '');
-            errors.innerHTML = 'Please Enter valid City Name';
-        });
-    errors.innerHTML = '';
+    try {
+        const response = await data(cityName);
+        const responseData = await response.json();
+
+        degree.innerHTML = `${ConvertToCelsius(responseData.main.temp)}&deg;C`;
+        humidity.innerHTML = `Humidity: ${(responseData.main.humidity) / 100 * 100}%`;
+        feelslike.innerHTML = `Feels like: ${ConvertToCelsius(responseData.main.feels_like)}&deg;C`;
+        maxtemp.innerHTML = `Maximum Temperature: ${ConvertToCelsius(responseData.main.temp_max)}&deg;C`;
+        mintemp.innerHTML = `Minimum Temperature: ${ConvertToCelsius(responseData.main.temp_min)}&deg;C`;
+        windspeed.innerHTML = `Wind Speed: ${Math.round(responseData.wind.speed * 3.6)} km/h`;
+        DataError.innerHTML = '';
+
+    } catch (error) {
+        console.error(error);
+        document.querySelectorAll('.all').forEach((e) => e.innerHTML = '');
+        DataError.innerHTML = 'Please Enter valid City Name';
+    }
 }
 
 const ReloadPage = () => {
