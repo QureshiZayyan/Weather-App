2d299b4fbe9ec52a8889ac7bba4a324300cb69c4
zayyan <qureshizayyan49@gmail.com>
2024-10-07 01:59:27 +0530

    fixed some bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,9 +8,11 @@ const Card = () => {
 
     const { query, weatherdata, setWeatherdata, setCountry, country } = useContext(StateContext);
     const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchWeatherData = async () => {
+            setLoading(true);
             setError('');
             try {
                 const url = `https://weather-api138.p.rapidapi.com/weather?city_name=${query}`;
@@ -29,11 +31,13 @@ const Card = () => {
                 const data = await response.json();
                 setWeatherdata(data);
                 setCountry(data.sys.country);
+                setLoading(false)
                 console.log(data);
 
             } catch (error) {
                 console.error(error);
                 setError('Please enter a valid city name');
+                setLoading(false);
                 setWeatherdata(null);
                 setCountry('');
             }
@@ -47,41 +51,48 @@ const Card = () => {
     };
 
     return (
+        <>
+            <div className="card rounded-5 m-auto position-relative">
+                <div className="card-header text-white bg-black rounded-5 d-flex align-items-center justify-content-center text-center">
+                    <h3 className="fw-bold" id="city-name">
+                        <MdLocationPin className='location' size={40} />
+                        {query} {country ? `, ${country}` : ''}
+                    </h3>
+                </div>
+                <div className="card-body px-2 text-bold">
+                    {
+                        loading ? (
+                            <img src={loader} alt="" className='icon' />
+                        )
+                            : error ?
+                                (
+                                    <>
+                                        <p>{error}</p>
+                                        <img src={errorpng} alt="" className='icon' />
+                                    </>
+                                )
+                                : weatherdata ?
+                                    (
+                                        <>
+                                            <h1 className="card-title" id="degree">
+                                                {convertToCelsius(weatherdata.main.temp)}&deg;C
+                                            </h1>
+                                            <ul className="list-unstyled mt-3 mb-4">
+                                                <li id="humidity" className="all">Humidity : {weatherdata.main.humidity}%</li>
+                                                <li id="feelslike" className="all">Feels Like : {convertToCelsius(weatherdata.main.feels_like)}&deg;C</li>
+                                                <li id="maxtemp" className="all">Max Temp : {convertToCelsius(weatherdata.main.temp_max)}&deg;C</li>
+                                                <li id="mintemp" className="all">Min Temp : {convertToCelsius(weatherdata.main.temp_min)}&deg;C </li>
+                                                <li id="raininfo" className="all">Rain Info : {weatherdata.weather[0].description}</li>
+                                            </ul>
+                                        </>
+                                    )
+                                    :
+                                    null
+                    }
 
-        <div className="card rounded-5 m-auto position-relative">
-            <div className="card-header text-white bg-black rounded-5 d-flex align-items-center justify-content-center text-center">
-                <h3 className="fw-bold" id="city-name">
-                    <MdLocationPin className='location' size={40} />
-                    {query} {country ? `, ${country}` : ''}
-                </h3>
+                </div>
             </div>
-            <div className="card-body px-2 text-bold">
-                {error ?
-                    (<>
-                        <p>{error}</p>
-                        <img src={errorpng} alt="" className='icon' />
-                    </>)
-                    :
-                    !weatherdata ? (
-                        <img src={loader} alt="" className='icon' />
-                    )
-                        :
-                        (
-                            <>
-                                <h1 className="card-title" id="degree">
-                                    {convertToCelsius(weatherdata.main.temp)}&deg;C
-                                </h1>
-                                <ul className="list-unstyled mt-3 mb-4">
-                                    <li id="humidity" className="all">Humidity : {weatherdata.main.humidity}%</li>
-                                    <li id="feelslike" className="all">Feels Like : {convertToCelsius(weatherdata.main.feels_like)}&deg;C</li>
-                                    <li id="maxtemp" className="all">Max Temp : {convertToCelsius(weatherdata.main.temp_max)}&deg;C</li>
-                                    <li id="mintemp" className="all">Min Temp : {convertToCelsius(weatherdata.main.temp_min)}&deg;C </li>
-                                    <li id="raininfo" className="all">Rain Info : {weatherdata.weather[0].description}</li>
-                                </ul>
-                            </>
-                        )}
-            </div>
-        </div>
+        </>
     )
 }
 
